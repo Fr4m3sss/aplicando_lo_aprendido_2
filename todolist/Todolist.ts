3fb43a82f0974aadd834bce3f15ec5db66369195
Marcos <colomamarcos777@gmail.com>
2025-10-06 20:01:46 -0300

    todolist primer commit

--- todolist/Todolist.ts
+++ todolist/Todolist.ts
@@ -1,2 +1,48 @@
-const message: string = "chau";
-console.log(message);
+const readline = require("readline/promises");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+type Status = "pendiente" | "en progreso" | "completado" | "";
+type Difficulty = "facil" | "medio" | "dificil" | "";
+
+interface Task {
+    title: string;
+    description: string;
+    status: Status;
+    difficulty: Difficulty;
+    createdAt: Date;
+    dueDate: Date | null;
+    lastEdited?: Date | null;
+}
+
+//----------Array global de tareas---------//
+
+const tasks: Task[] = [
+    {
+        title: "Aprender Node.js",
+        description: "Revisar documentación y practicar ejemplos",
+        status: "pendiente",
+        difficulty: "facil",
+        createdAt: new Date(),
+        dueDate: null
+    },
+    {
+        title: "Aprender Java",
+        description: "Revisar documentación y practicar ejemplos",
+        status: "en progreso",
+        difficulty: "medio",
+        createdAt: new Date(),
+        dueDate: null
+    },
+    {
+        title: "Aprender Typescript",
+        description: "Revisar documentación y practicar ejemplos",
+        status: "completado",
+        difficulty: "dificil",
+        createdAt: new Date(),
+        dueDate: null
+    }
+];
